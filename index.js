b321ef88cd9e973c1873dafceae82681b064d749
saleh-expansionjs <saleh@expansionjs.com>
2025-12-31 04:15:14 +0530

    final check

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(cors({
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('Inframe Design Institute Backend is running with cicd')
+    res.send('Inframe Design Institute Backend is running')
 })
 
 app.use('/web', websiteRoute)
